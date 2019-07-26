9b8f41093b9f0fb346dfa843448b53ca5412ffea
Mark-PC/Mark <mark@healyconsultants.com>
2019-07-26 18:05:46 +0800

    Fixes Footer warning: each child in an array should have a unique key prop

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -64,8 +64,8 @@ const Footer = ({ data, children }) => (
   <FooterGroup>
     <Text>Tweet “Prototype and build apps with React and Swift. New courses by @MengTo”</Text>
     <Button>Tweet</Button>
-    <LinkGroup>{ data.allContentfulLink.edges.map((edge) => (
-        <a href={edge.node.url}>{edge.node.title}</a>
+    <LinkGroup>{ data.allContentfulLink.edges.map((edge, index) => (
+        <a key={index} href={edge.node.url}>{edge.node.title}</a>
       ))}</LinkGroup>
     <Copyright>{children}</Copyright>
   </FooterGroup>
